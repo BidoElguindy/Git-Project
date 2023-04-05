19a92215476f943be542b6c3742bb4567d723b8e
BidoElguindy <92274618+BidoElguindy@users.noreply.github.com>
2023-04-05 15:50:42 +0200

    Update and rename ex4.c to ex4.h

---
+++ ex4.h
@@ -0,0 +1,17 @@
+
+#ifndef EX4_h
+#define EX4_h
+
+typedef struct {
+char * name ;
+char * hash ;
+int mode ;
+} WorkFile ;
+
+typedef struct {
+WorkFile * tab ;
+int size ;
+int n ;
+} WorkTree ;
+
+#endif
