74f96dd083fe4c4217936d7ee15fb7bfdafbf566
BidoElguindy <92274618+BidoElguindy@users.noreply.github.com>
2023-04-11 16:22:45 +0200

    Update ex4.c

--- ex4.c
+++ ex4.c
@@ -5,24 +5,6 @@
 #include "ex4.h"
 #define N_WF 20
 
-/*
-int getChmod(const char* path) {
-    struct stat ret;
-
-    if (stat(path, &ret) == -1) {
-        return -1;
-    }
-
-    return (ret.st_mode & S_IRUSR) | (ret.st_mode & S_IWUSR) | (ret.st_mode & S_IXUSR) | 
-           (ret.st_mode & S_IRGRP) | (ret.st_mode & S_IWGRP) | (ret.st_mode & S_IXGRP) |
-           (ret.st_mode & S_IROTH) | (ret.st_mode & S_IWOTH) | (ret.st_mode & S_IXOTH); 
-}
-*/
-void setMode (int mode , char * path ) {
-    char buff [100];
-    sprintf ( buff , "chmod %d %s ", mode , path ) ;
-    system ( buff ) ;
-    }
 
 
 WorkFile* createWorkFile(char* name){
