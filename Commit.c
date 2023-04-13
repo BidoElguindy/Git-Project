49e12ee4b21722d77dbae1e019a3ec3f1bdccf4c
Carole <99185950+carolita03@users.noreply.github.com>
2023-04-13 19:00:50 +0200

    Update Commit.c

--- Commit.c
+++ Commit.c
@@ -1,6 +1,10 @@
 #include "commit.h"
 #include "Work.h"
 
+
+//EXERCICE 6
+
+
 kvp* createKeyVal(char* key, char* val) {
     kvp* k = malloc(sizeof(kvp)); // Allocation de la mémoire pour une nouvelle paire clé-valeur
     k->key = strdup(key); // Copie de la clé dans la structure
@@ -8,12 +12,12 @@ kvp* createKeyVal(char* key, char* val) {
     return k; // Retourne la nouvelle paire clé-valeur allouée
 }
 
-
 void freeKeyVal(kvp* kv) {
     free(kv->key);
     free(kv->value);
     free(kv);
 }
+
 
 char* kvts(kvp* k) {
     // Allocation de la mémoire pour le buffer
@@ -24,33 +28,31 @@ char* kvts(kvp* k) {
     return buff;
 }
 
-
-
 kvp* stkv(char* str) {
     // On utilise la fonction strtok pour extraire la clé et la valeur de la chaîne.
-    // on récupère la première sous-chaîne jusqu'au premier délimiteur ':'.
+    // On récupère la première sous-chaîne jusqu'au premier délimiteur ':'.
     char* key = strtok(str, ":");
-    // Cette fois-ci, on récupère la deuxième sous-chaîne à partir du délimiteur ':'.
+    // On récupère la deuxième sous-chaîne à partir du délimiteur ':'.
     char* value = strtok(NULL, ":");
-    // Enfin, on crée la structure kvp à partir de la clé et de la valeur extraites.
+    // On crée la structure kvp à partir de la clé et de la valeur extraites.
     return createKeyVal(key, value);
 }
 
+
 Commit* initCommit() {
-    // alloue de l'espace pour la structure de Commit
+    // Allocation de l'espace pour la structure de Commit
     Commit* c = malloc(sizeof(Commit));
-    // initialise les champs de la structure Commit
+    // Initialisation les champs de la structure Commit
     c->n = 0;
     c->size = COMMIT_SIZE;
-    // alloue de l'espace pour le tableau de pointeurs de type kvp*
+    // Allocation de l'espace pour le tableau de pointeurs de type kvp*
     c->T = malloc(sizeof(kvp*) * COMMIT_SIZE);
-    // initialise chaque pointeur du tableau à NULL
+    // Initialisation de chaque pointeur du tableau à NULL
     for (int i = 0; i < COMMIT_SIZE; i++) {
         c->T[i] = NULL;
     }
-    return c; // renvoie un pointeur vers la structure de Commit initialisée
+    return c; // Renvoie un pointeur vers la structure de Commit initialisée
 }
-
 
 unsigned long hashF(char * str){
     unsigned long hash = 0;
@@ -92,54 +94,46 @@ Commit* createCommit(char* hash) {
 }
 
 char* commitGet(Commit* c, char* key) {
-    int index = hashF(key) % c->size;  // calcul de l'indice initial
+    int index = hashF(key) % c->size;  // Calcul de l'indice initial
     int i = 0;
-    while (c->T[index] != NULL && i < c->size) { // tant que la case n'est pas vide et qu'on a pas parcouru toutes les cases
-        if (strcmp(c->T[index]->key, key) == 0) { // si la clé est trouvée
-            return c->T[index]->value;  // on retourne la valeur correspondante
+    while (c->T[index] != NULL && i < c->size) { // Tant que la case n'est pas vide et qu'on a pas parcouru toutes les cases
+        if (strcmp(c->T[index]->key, key) == 0) { // Si la clé est trouvée
+            return c->T[index]->value;  // On retourne la valeur correspondante
         }
         i++;
-        index = (index + 1) % c->size; // on avance à la case suivante avec le probing linéaire
+        index = (index + 1) % c->size; // On avance à la case suivante avec le probing linéaire
     }
-    return NULL;  // la clé n'a pas été trouvée
+    return NULL;  // La clé n'a pas été trouvée
 }
 
-/*
- * La fonction cts prend en entrée un pointeur vers un Commit, et retourne une chaîne de caractères représentant ce Commit.
- * La chaîne de caractères retournée est composée des chaînes de caractères représentant chacun des couples (clé, valeur), séparées par un saut de ligne.
- */
+
 char* cts(Commit* c) {
     char* str = malloc(sizeof(char) * 1000);
     str[0] = '\0'; // Initialisation de la chaîne de caractères vide
     for (int i = 0; i < c->size; i++) {
         if (c->T[i] != NULL) {
-            char* kv_str = kvts(c->T[i]); // Convertir le couple clé-valeur en une chaîne de caractères
-            strcat(str, kv_str); // Ajouter la chaîne de caractères représentant le couple clé-valeur à la chaîne résultante
-            strcat(str, "\n"); // Ajouter un saut de ligne entre les couples
-            free(kv_str); // Libérer la mémoire allouée pour la chaîne de caractères représentant le couple clé-valeur
+            char* kv_str = kvts(c->T[i]); // Conversion du couple clé-valeur en une chaîne de caractères
+            strcat(str, kv_str); // Ajout de la chaîne de caractères représentant le couple clé-valeur à la chaîne résultante
+            strcat(str, "\n"); // Ajout d'un saut de ligne entre les couples
+            free(kv_str); // Libération de la mémoire allouée pour la chaîne de caractères représentant le couple clé-valeur
         }
     }
     return str;
 }
 
-/*
- * La fonction stc prend en entrée une chaîne de caractères représentant un Commit, et retourne un pointeur vers le Commit représenté par la chaîne de caractères.
- * La chaîne de caractères doit être composée des chaînes de caractères représentant chacun des couples (clé, valeur), séparées par un saut de ligne.
- */
 Commit* stc(char* ch) {
-    Commit* c = initCommit(); // Initialiser un nouveau Commit vide
-    char* tok = strtok(ch, "\n"); // Découper la chaîne de caractères en sous-chaînes représentant chaque couple (clé, valeur)
+    Commit* c = initCommit(); // Initialisation d'un nouveau Commit vide
+    char* tok = strtok(ch, "\n"); // Découpage de la chaîne de caractères en sous-chaînes représentant chaque couple (clé, valeur)
     while (tok != NULL) {
-        kvp* kv = stkv(tok); // Convertir la sous-chaîne représentant un couple (clé, valeur) en une structure kvp
-        commitSet(c, kv->key, kv->value); // Ajouter le couple (clé, valeur) au Commit
-        free(kv->key); // Libérer la mémoire allouée pour la clé
-        free(kv->value); // Libérer la mémoire allouée pour la valeur
-        free(kv); // Libérer la mémoire allouée pour la structure kvp
-        tok = strtok(NULL, "\n"); // Passer à la sous-chaîne suivante
+        kvp* kv = stkv(tok); // Conversion de la sous-chaîne représentant un couple (clé, valeur) en une structure kvp
+        commitSet(c, kv->key, kv->value); // Ajout du couple (clé, valeur) au Commit
+        free(kv->key); // Libération de la mémoire allouée pour la clé
+        free(kv->value); // Libération de la mémoire allouée pour la valeur
+        free(kv); // Libération de la mémoire allouée pour la structure kvp
+        tok = strtok(NULL, "\n"); // Passage à la sous-chaîne suivante
     }
     return c;
 }
-
 
 
 void ctf(Commit* c, char* file) {
@@ -185,13 +179,14 @@ Commit* ftc(char* file) {
     return c;
 }
 
+
 char *blobCommit(Commit *c) {
     char fname[100] = "/tmp/myfileXXXXXX";
     mkstemp(fname);
     ctf(c, fname);
     char *hash = sha256file(fname);
     char *path = hashtoPath(hash);
-    char *ret = strdup(hash); // sauvegarde du hash pour rendu de fonction
+    char *ret = strdup(hash); // Sauvegarde du hash pour rendu de fonction
 	hash[2] = '\0'; // 2 premiers cara du hash pour mkdir
 	char output[1000];
 	if (!file_exists(hash)) {
@@ -202,7 +197,7 @@ char *blobCommit(Commit *c) {
 	sprintf(output,"touch %s.c",path);
 	system(output);
 	sprintf(output,"%s.c",path);
-	cp(output,fname); // copie du contenu de wt vers le fichier .t
+	cp(output,fname); // Copie du contenu de wt vers le fichier .t
     printf("commit sauvegarde dans %s\n",path);
 	free(path);
     return ret;
@@ -221,13 +216,13 @@ void freeCommit(Commit *c) {
 
 
 int main() {
-    // Testing createKeyVal and freeKeyVal
+    // Test createKeyVal et freeKeyVal
     printf("Testing createKeyVal and freeKeyVal...\n");
     kvp* kv = createKeyVal("key", "value");
     printf("Key: %s, Value: %s\n", kv->key, kv->value);
     free(kv);
 
-    // Testing kvts and stkv
+    // Test kvts et stkv
     printf("Testing kvts and stkv...\n");
     char* kv_string = "key=value\n";
     kvp* kv2 = stkv(kv_string);
@@ -237,7 +232,7 @@ int main() {
     free(kv2);
     free(kv_string2);
 
-    // Testing initCommit and commitSet
+    // Test initCommit et commitSet
     printf("Testing initCommit and commitSet...\n");
     Commit* c = initCommit(10);
     commitSet(c, "key1", "value1");
@@ -245,7 +240,7 @@ int main() {
     printf("Value for key1: %s\n", commitGet(c, "key1"));
     printf("Value for key2: %s\n", commitGet(c, "key2"));
 
-    // Testing createCommit
+    // Test createCommit
     printf("Testing createCommit...\n");
     Commit* c2 = createCommit("hash123");
     commitSet(c2, "key3", "value3");
@@ -253,31 +248,31 @@ int main() {
     printf("Commit string: %s\n", commit_string);
     free(commit_string);
 
-    // Testing commitGet
+    // Test commitGet
     printf("Testing commitGet...\n");
     char* value = commitGet(c2, "key3");
     printf("Value for key3: %s\n", value);
     free(value);
 
-    // Testing stc
+    // Test stc
     printf("Testing stc...\n");
     Commit* c3 = stc(commit_string);
     printf("Value for key3: %s\n", commitGet(c3, "key3"));
     free(commit_string);
 
-    // Testing ctf and ftc
+    // Test ctf et ftc
     printf("Testing ctf and ftc...\n");
     ctf(c2, "test_commit.txt");
     Commit* c4 = ftc("test_commit.txt");
     printf("Value for key3: %s\n", commitGet(c4, "key3"));
 
-    // Testing blobCommit
+    // Test blobCommit
     printf("Testing blobCommit...\n");
     char* hash = blobCommit(c2);
     printf("Blob hash: %s\n", hash);
     free(hash);
 
-    // Cleanup
+    // Libération de la mémoire
     freeCommit(c);
     freeCommit(c2);
     freeCommit(c3);
