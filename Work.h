0202f910709e0235ff18323f6a674e964cb33d76
Carole <99185950+carolita03@users.noreply.github.com>
2023-04-13 22:09:37 +0200

    Update Work.h

--- Work.h
+++ Work.h
@@ -12,16 +12,16 @@
 
 
 typedef struct {
-char * name ;
-char * hash ;
-int mode ;
-} WorkFile ;
+    char * name;
+    char * hash;
+    int mode;
+} WorkFile;
 
 typedef struct {
-WorkFile * tab ;
-int size ;
-int n ;
-} WorkTree ;
+    WorkFile * tab;
+    int size;
+    int n;
+} WorkTree;
 
 //EXERCICE 4 
 
