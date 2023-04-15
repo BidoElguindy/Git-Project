6e2f9268e2ef13d424a19ccc42be45523fdb9e5d
BidoElguindy <92274618+BidoElguindy@users.noreply.github.com>
2023-04-15 16:14:24 +0200

    Update Refs.h

--- Refs.h
+++ Refs.h
@@ -1,22 +1,20 @@
 #ifndef _REFS_H_
 #define _REFS_H_
-#include "commit.h"
+#include "Commit.h"
 
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 
 /*initialise le repertoire .refs*/
 void initRefs();
 
 /*cree et/ou met a jour la ref ref_name avec le hash donne*/
-void createUpdateRefs(char *ref_name, char *hash);
+void createUpdateRef(char *ref_name, char *hash);
+ 
 
 /*supprime la ref donnee*/
-void deleteRefs(char *ref_name);
+void deleteRef(char* ref_name);
 
 /*renvoie le hash de la ref donnee*/
-char* getRefs(char* ref_name);
+char* getRef(char* ref_name);
 
 /*raccourci pour creer un fichier avec touch*/
 void createFile(char *file);
@@ -26,6 +24,8 @@ void myGitAdd(char *file_or_folder);
 
 /*cree un commit et le stocke dans la branche (ref) donnee*/
 void myGitCommit(char* branch_name, char* message);
+
+
 
 
 #endif 
