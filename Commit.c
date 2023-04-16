94f92bbe80d04a88dcc6fc38115e01f752f26104
Carole <99185950+carolita03@users.noreply.github.com>
2023-04-16 17:57:04 +0200

    Update Commit.c

--- Commit.c
+++ Commit.c
@@ -6,16 +6,46 @@
 
 
 kvp* createKeyVal(char* key, char* val) {
-    kvp* k = malloc(sizeof(kvp)); // Allocation de la mémoire pour une nouvelle paire clé-valeur
-    k->key = strdup(key); // Copie de la clé dans la structure
-    k->value = strdup(val); // Copie de la valeur dans la structure
-    return k; // Retourne la nouvelle paire clé-valeur allouée
+    // Allouer de la mémoire pour la nouvelle paire
+    kvp* kv = malloc(sizeof(kvp));
+    if (kv == NULL) {
+        // Impossible d'allouer de la mémoire pour la paire
+        return NULL;
+    }
+    // Copier la clé dans la paire allouée dynamiquement
+    kv->key = strdup(key);
+    if (kv->key == NULL) {
+        // Impossible de copier la clé, libérer la mémoire allouée pour la paire
+        free(kv);
+        return NULL;
+    }
+    // Copier la valeur dans la paire allouée dynamiquement
+    kv->value = strdup(val);
+    if (kv->value == NULL) {
+        // Impossible de copier la valeur, libérer la mémoire allouée pour la clé et la paire
+        free(kv->key);
+        free(kv);
+        return NULL;
+    }
+    // Retourner la paire nouvellement créée
+    return kv;
 }
 
+
 void freeKeyVal(kvp* kv) {
+    // Vérifier que la paire n'est pas nulle
     if (kv != NULL){
-        free(kv->key);
-        free(kv->value);
+        // Vérifier que la clé n'est pas nulle
+        if (kv->key != NULL) {
+            // Libérer la mémoire allouée pour la clé
+            free(kv->key);
+        }
+        // Vérifier que la valeur n'est pas nulle
+        if (kv->value != NULL) {
+            // Libérer la mémoire allouée pour la valeur
+            free(kv->value);
+        }
+        // Libérer la mémoire allouée pour la paire
         free(kv);
     }
 }
@@ -45,15 +75,25 @@ kvp* stkv(char* str) {
 }
 
 
-
 Commit* initCommit() {
     // Allocation de l'espace pour la structure de Commit
     Commit* c = malloc(sizeof(Commit));
+    // Vérification de l'allocation de mémoire pour la structure Commit
+    if (c == NULL) {
+        fprintf(stderr, "Erreur : échec de l'allocation de mémoire pour le commit\n");
+        return NULL;
+    }
     // Initialisation les champs de la structure Commit
     c->n = 0;
     c->size = COMMIT_SIZE;
     // Allocation de l'espace pour le tableau de pointeurs de type kvp*
     c->T = malloc(sizeof(kvp*) * COMMIT_SIZE);
+    // Vérification de l'allocation de mémoire pour le tableau de pointeurs
+    if (c->T == NULL) {
+        fprintf(stderr, "Erreur : échec de l'allocation de mémoire pour le tableau de pointeurs\n");
+        free(c);
+        return NULL;
+    }
     // Initialisation de chaque pointeur du tableau à NULL
     for (int i = 0; i < COMMIT_SIZE; i++) {
         c->T[i] = NULL;
@@ -62,39 +102,43 @@ Commit* initCommit() {
 }
 
 unsigned long hashF(char * str){
-    unsigned long hash = 0;
-    int c;
-    while((c = *str++)!='\0'){
-        hash = c + (hash << 6) + (hash << 16) - hash;
+    unsigned long hash = 0; // initialise la valeur du hachage à 0
+    int c; // déclare une variable entière pour stocker la valeur ASCII de chaque caractère de la chaîne
+    while((c = *str++)!='\0'){ // parcourt les caractères de la chaîne jusqu'à ce que le caractère nul soit rencontré
+        hash = c + (hash << 6) + (hash << 16) - hash; // calcule la valeur du hachage en utilisant sdbm
     }
-    return hash;
+    return hash; // retourne la valeur de hachage calculée
 }
 
+
 void commitSet(Commit* c, char* key, char* value) {
-    unsigned long index = hashF(key) % c->size; // Calcul de l'indice à partir de la clé
-    unsigned long i = index;
-    do {
-        if (c->T[i] == NULL || strcmp(c->T[i]->key, key) == 0) { // Si la case est vide ou contient la clé cherchée
-            c->T[i] = createKeyVal(key, value); // On insère la paire dans la case courante
-            c->n++;
-            return;
+    int p = hashF(key) % c->size; // obtenir l'index où stocker la paire clé-valeur en utilisant la fonction de hachage "hashF"
+    while (c->T[p] != NULL) { // boucler jusqu'à trouver un emplacement vide dans la table de hachage
+        if (strcmp(c->T[p]->key, key) == 0) { // si une paire clé-valeur avec la clé donnée existe déjà dans la table de hachage, libérer la mémoire
+            freeKeyVal(c->T[p]);
+            break;
         }
-        i = (i + 1) % c->size; // Prochaine case, avec wrapping
-    } while (i != index); // Tant qu'on n'a pas fait un tour complet
-    fprintf(stderr, "Error: table de hachage pleine, impossible d'insérer la paire (%s,%s)\n", key, value);
+        p = (p+1) % c->size; // probing linéaire : passer à l'emplacement suivant en utilisant une méthode de sondage linéaire
+    }
+    kvp* pair = createKeyVal(key, value); // créer une nouvelle paire clé-valeur avec la clé et la valeur données
+    if (pair != NULL) { // si la paire clé-valeur a été créée avec succès
+        c->T[p] = pair; // stocker la paire clé-valeur à l'emplacement approprié dans la table de hachage
+        c->n++; // incrémenter le nombre de paires clé-valeur stockées dans la table de hachage
+    }
 }
 
 Commit* createCommit(char* hash){ 
-    Commit* c = initCommit(); 
-    commitSet(c,"tree", hash);
-    return c;
+    Commit* c = initCommit(); // crée un nouvel objet Commit en appelant la fonction initCommit()
+    commitSet(c,"tree", hash); // définit la clé "tree" avec la valeur de hachage donnée en utilisant la fonction commitSet()
+    return c; // retourne l'objet Commit créé
 }
+
 
 char* commitGet(Commit* c, char* key) {
     int index = hashF(key) % c->size;  // Calcul de l'indice initial
     int i = 0;
-    while (c->T[index] != NULL && i < c->size) { // Tant que la case n'est pas vide et qu'on a pas parcouru toutes les cases
-        if (strcmp(c->T[index]->key, key) == 0) { // Si la clé est trouvée
+    while (i < c->size && c->T[index] != NULL) { // Tant que la case n'est pas vide et qu'on a pas parcouru toutes les cases
+        if (c->T[index] != NULL && strcmp(c->T[index]->key, key) == 0) { // Si la clé est trouvée
             return c->T[index]->value;  // On retourne la valeur correspondante
         }
         i++;
@@ -103,33 +147,36 @@ char* commitGet(Commit* c, char* key) {
     return NULL;  // La clé n'a pas été trouvée
 }
 
-
-char* cts(Commit* c) {
-    char* str = malloc(sizeof(char) * 1000);
-    str[0] = '\0'; // Initialisation de la chaîne de caractères vide
-    for (int i = 0; i < c->size; i++) {
-        if (c->T[i] != NULL) {
-            char* kv_str = kvts(c->T[i]); // Conversion du couple clé-valeur en une chaîne de caractères
-            strcat(str, kv_str); // Ajout de la chaîne de caractères représentant le couple clé-valeur à la chaîne résultante
-            strcat(str, "\n"); // Ajout d'un saut de ligne entre les couples
-            free(kv_str); // Libération de la mémoire allouée pour la chaîne de caractères représentant le couple clé-valeur
+char* cts(Commit* c){
+    char* res= (char*)malloc(sizeof(char)*KVTS_SIZE*c->n); // alloue de la mémoire pour stocker les paires clé-valeur sous forme de chaîne de caractères
+    res[0]='\0'; // initialiser la chaîne de caractères résultat à la chaîne vide
+    char * kvtss = NULL;
+    for(int i=0; i< c->size ; i++){ // boucle à travers tous les emplacements de la table de hachage
+        if(c->T[i] != NULL){ // si une paire clé-valeur est stockée à l'emplacement actuel
+            kvtss = kvts(c->T[i]); // convertir la paire clé-valeur en une chaîne de caractères
+            strcat(res, kvtss); // concaténer la chaîne de caractères résultat avec la chaîne de caractères représentant la paire clé-valeur
+            strcat(res,"\n"); // ajouter un saut de ligne à la fin de chaque paire clé-valeur
+            free(kvtss); // libérer la mémoire allouée pour la chaîne de caractères représentant la paire clé-valeur
         }
     }
-    return str;
+    return res; // retourne la chaîne de caractères résultat contenant toutes les paires clé-valeur stockées dans la table de hachage
 }
 
-Commit* stc(char* ch) {
-    Commit* c = initCommit(); // Initialisation d'un nouveau Commit vide
-    char* tok = strtok(ch, "\n"); // Découpage de la chaîne de caractères en sous-chaînes représentant chaque couple (clé, valeur)
-    while (tok != NULL) {
-        kvp* kv = stkv(tok); // Conversion de la sous-chaîne représentant un couple (clé, valeur) en une structure kvp
-        commitSet(c, kv->key, kv->value); // Ajout du couple (clé, valeur) au Commit
-        free(kv->key); // Libération de la mémoire allouée pour la clé
-        free(kv->value); // Libération de la mémoire allouée pour la valeur
-        free(kv); // Libération de la mémoire allouée pour la structure kvp
-        tok = strtok(NULL, "\n"); // Passage à la sous-chaîne suivante
+
+Commit *stc(char *str) {
+    Commit *c = initCommit(); // initialiser un nouveau commit vide
+    char *key;
+    char *value;
+    char *line = strtok(str, "\n"); // divise la chaîne de caractères "str" en lignes individuelles en utilisant "\n" comme délimiteur
+    
+    while (line != NULL) { // boucle à travers chaque ligne dans la chaîne de caractères "str"
+        key = strtok(line, ":"); // divise la ligne actuelle en deux parties: clé et valeur, en utilisant ":" comme délimiteur
+        value = strtok(NULL, ":");
+        commitSet(c, key, value); // ajoute la paire clé-valeur à la table de hachage du commit
+        line = strtok(NULL, "\n"); // passer à la ligne suivante
     }
-    return c;
+    
+    return c; // retourne le commit rempli avec les paires clé-valeur extraites de la chaîne de caractères "str"
 }
 
 
@@ -148,6 +195,9 @@ void ctf(Commit* c, char* file) {
 
     // Écriture de la chaîne de caractères dans le fichier
     fputs(commit_str, fp);
+
+    //Libération de la mémoire
+    free(commit_str);
 
     // Fermeture du fichier
     fclose(fp);
@@ -176,6 +226,7 @@ Commit* ftc(char* file) {
     return c;
 }
 
+
 char *blobCommit(Commit *c) {
     char fname[100] = "/tmp/myfileXXXXXX"; // Initialise une chaîne de caractères fname avec un chemin vers un fichier temporaire
     mkstemp(fname); // Crée un fichier temporaire vide avec le chemin fname
@@ -199,6 +250,32 @@ char *blobCommit(Commit *c) {
     return ret; // Renvoie le hash SHA-256 calculé
 }
 
+
+void printCommit(Commit* c) {
+    printf("Commit with %d entries:\n", c->n); // affiche le nombre de paires clé-valeur stockées dans le commit
+    for (int i = 0; i < c->size; i++) { // boucle à travers tous les emplacements de la table de hachage
+        if (c->T[i] != NULL) { // si une paire clé-valeur est stockée à l'emplacement actuel, l'afficher
+            printf("%s: %s\n", c->T[i]->key, c->T[i]->value);
+        }
+    }
+}
+
+
+void commitDeleteValue(Commit* c, char* value) {
+    // parcours de toutes les cases de la table de hachage
+    for (int i = 0; i < c->size; i++) {
+        // si la case est occupée et que la valeur correspond
+        if (c->T[i] != NULL && strcmp(c->T[i]->value, value) == 0) {
+            // on supprime la paire clé-valeur de la table
+            freeKeyVal(c->T[i]);
+            c->T[i] = NULL;
+            c->n--;
+            return;
+        }
+    }
+}
+
+
 void freeCommit(Commit *c) {
     if (c!=NULL) { // Vérifie si le Commit c n'est pas NULL
         for (int i=0;i<c->n;i++) { // Parcourt tous les éléments de la table de hachage du Commit c
@@ -209,75 +286,3 @@ void freeCommit(Commit *c) {
     free(c); // Libère la mémoire allouée pour le Commit c
 }
 
-
-/*
-
-int main() {
-    printf("Test Exercice 6 \n");
-    printf("\n");
-    
-     // Test createKeyVal et freeKeyVal
-    printf("Testing createKeyVal and freeKeyVal \n");
-    kvp* kv = createKeyVal("key", "value");
-    printf("Key: %s, Value: %s\n", kv->key, kv->value);
-    free(kv);
-
-    // Test kvts et stkv
-    printf("Testing kvts and stkv\n");
-    char* kv_string = "key: value\n";
-    kvp* kv2 = stkv(kv_string);  
-    printf("Key: %s, Value: %s\n", kv2->key, kv2->value);
-    char* kv_string2 = kvts(kv2);
-    printf("String: %s\n", kv_string2);
-    free(kv2);
-    free(kv_string2);
-
-    // Test initCommit et commitSet
-    printf("Testing initCommit and commitSet\n");
-    Commit* c = initCommit();
-    commitSet(c, "key1", "value1");
-    commitSet(c, "key2", "value2");
-    printf("Value for key1: %s\n", commitGet(c, "key1"));
-    printf("Value for key2: %s\n", commitGet(c, "key2"));
-
-    // Test createCommit
-    printf("Testing createCommit\n");
-    Commit* c2 = createCommit("hash123");
-    commitSet(c2, "key3", "value3");
-    char* commit_string = cts(c2);
-    printf("Commit string: %s\n", commit_string);
-
-    // Test commitGet
-    printf("Testing commitGet\n");
-    char* value = commitGet(c2, "key3");
-    printf("Value for key3: %s\n", value);
-    free(value);
-
-    // Test stc
-    printf("Testing stc\n");
-    Commit* c3 = stc(commit_string);
-    printf("Value for key3: %s\n", commitGet(c3, "key3"));
-    free(commit_string);
-
-    // Test ctf et ftc
-    printf("Testing ctf and ftc\n");
-    ctf(c3, "test_commit.txt");
-    Commit* c4 = ftc("test_commit.txt");
-    printf("Value for key3: %s\n", commitGet(c4, "key3"));
-
-    // Test blobCommit
-    printf("Testing blobCommit\n");
-    char* hash = blobCommit(c2);
-    printf("Blob hash: %s\n", hash);
-    free(hash);
-
-    // Libération de la mémoire
-    freeCommit(c);
-    freeCommit(c2);
-    freeCommit(c3);
-    freeCommit(c4);
-     
-    return 0;
-}
-
-*/
