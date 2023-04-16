c033dd6dac80665754bdd64e4ef204dde5897bcb
Carole <99185950+carolita03@users.noreply.github.com>
2023-04-16 18:02:49 +0200

    Update Refs.h

--- Refs.h
+++ Refs.h
@@ -1,31 +1,29 @@
 #ifndef _REFS_H_
 #define _REFS_H_
+
 #include "Commit.h"
 
 
-/*initialise le repertoire .refs*/
+/* Q.1 La fonction iniRefs initialise le repertoire .refs */
 void initRefs();
 
-/*cree et/ou met a jour la ref ref_name avec le hash donne*/
+/* Q.2 La fonction createUpdateRef cree et/ou met a jour la ref ref_name avec le hash donne */
 void createUpdateRef(char *ref_name, char *hash);
  
-
-/*supprime la ref donnee*/
+/* Q.3 La fonction deleteRef supprime la ref donnee */
 void deleteRef(char* ref_name);
 
-/*renvoie le hash de la ref donnee*/
+/* Q.4 La fonction getRef renvoie le hash de la ref donnee */
 char* getRef(char* ref_name);
 
-/*raccourci pour creer un fichier avec touch*/
+/* Q.5 La fonction "createFile" crée un fichier vide avec le nom spécifié en utilisant la commande système "touch" */
 void createFile(char *file);
 
-/*ajoute le fichier ou repertoire donne dans le .add et le cree s'il n'existe pas*/
+/* Q.5 La fonction myGitAdd ajoute le fichier ou repertoire donne dans le .add et le cree s'il n'existe pas */
 void myGitAdd(char *file_or_folder);
 
-/*cree un commit et le stocke dans la branche (ref) donnee*/
+/* Q.6 La fonction myGitCommit cree un commit et le stocke dans la branche (ref) donnee */
 void myGitCommit(char* branch_name, char* message);
-
-
 
 
 #endif 
